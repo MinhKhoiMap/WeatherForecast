ba9b8b9ba963bde6c7e8fbf65cdb30c6c1130018
Thiện Thành Nguyễn <thanhoilathanh482@gmail.com>
2022-07-16 23:30:39 +0700

    xong login sign up con thieu location

--- src/Authentication/Signup/index.js
+++ src/Authentication/Signup/index.js
@@ -2,11 +2,11 @@ import React from 'react'
 import { useState } from 'react'
 import { app } from '../firebaseConfig';
 import {getAuth, signInWithEmailAndPassword, GoogleAuthProvider,signInWithPopup,
-    sendSignInLinkToEmail } from 'firebase/auth';
+    sendSignInLinkToEmail, createUserWithEmailAndPassword } from 'firebase/auth';
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import '../auth.css';
-const Login = () => {
+const Signup = () => {
     let auth = getAuth();
     let google = new GoogleAuthProvider();
     const [data, setData] = useState({
@@ -19,18 +19,23 @@ const Login = () => {
         setData({...data, ...newInput})
     }
     const handleSubmit = () => {
-        signInWithEmailAndPassword(auth, data.email, data.password)
-        .then((respone) => {
-            toast.success("Login successfully!!!");
-        })
-        .catch((error) => {
-            toast.error(error.message);
-        });
+        if (data.password === data.confirmPassword) {
+            createUserWithEmailAndPassword(auth, data.email, data.password)
+            .then((respone) => {
+                toast.success("success");
+            })
+            .catch((error) => {
+                toast.error(error.message);
+            });
+        }
+        else {
+            toast.error("Passwords don't match")
+        }
     }
     const googleAuth = () => {
         signInWithPopup(auth, google)
         .then((respone) => {
-            toast.success("Login successfully!!!");
+            toast.success("success");
             console.log(respone.user);
             sendSignInLinkToEmail(auth, respone.user.email, actionCodeSettings)
             .then(() => {
@@ -55,7 +60,7 @@ const Login = () => {
     return (
     <div className="auth-components">
         <div className='auth-container'>
-        <h1>Login</h1>
+        <h1>Register</h1>
             <div className="input-container">
                 <i class="fa-solid fa-user"></i>
                 <input onChange={handleInput} type="text" name='email' placeholder="Email" value={data.email} />
@@ -64,7 +69,11 @@ const Login = () => {
                 <i class="fa-solid fa-lock"></i>
                 <input onChange={handleInput} type="password" name="password" placeholder="Password" value={data.password} />
             </div>
-            <button onClick={handleSubmit} type="submit">Login</button>
+            <div className="input-container">
+                <i class="fa-solid fa-lock"></i>
+                <input onChange={handleInput} type="password" name="confirmPassword" placeholder="Confirm password" value={data.confirmPassword} />
+            </div>
+            <button onClick={handleSubmit} type="submit">Register</button>
             <button onClick={googleAuth} >Sign in with Google</button>
             <ToastContainer />
         </div>
@@ -72,4 +81,4 @@ const Login = () => {
     )
     }
 
-export default Login
+export default Signup
---
+++ src/Authentication/firebaseConfig.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDAk8dZKuGzWEOvcvipR2blUnGFRySfaDg",
+  authDomain: "test-8781e.firebaseapp.com",
+  projectId: "test-8781e",
+  storageBucket: "test-8781e.appspot.com",
+  messagingSenderId: "713208642558",
+  appId: "1:713208642558:web:db3f5a2feab0a2aa585eaf",
+  measurementId: "G-LWMB9CVM4D"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
